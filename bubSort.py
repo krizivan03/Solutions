e4fa55618267b8b47232287d18c3ff99f0e24203
Christopher Canales <55576199+krizivan03@users.noreply.github.com>
2020-01-22 23:57:39 -0700

    Update and rename p.py to bubSort.py

--- bubSort.py
+++ bubSort.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
-
+#this is a python implementation of Bubble Sort
 if __name__ == "__main__":
     list = [10,2,6,4,6,7]
-    count=1
 
     for i in range(len(list)):
         for j in range(len(list)-1):
